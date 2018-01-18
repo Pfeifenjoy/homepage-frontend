6682099360c0a464323e1fe7672c5aa4e8db2dee
Arwed Mett <arwed.mett@googlemail.com>
2018-01-18 20:37:59 +0100

    update

--- src/runtime.js
+++ src/runtime.js
@@ -5,15 +5,15 @@ import "babel-polyfill"
 import React from "react"
 import ReactDOM from "react-dom"
 import { Router, Route  } from "react-router"
-import createBrowserHistory from "history/createBrowserHistory"
+//import createBrowserHistory from "history/createBrowserHistory"
 import App from "./app"
 import { ThemeProvider } from "styled-components"
 import { Dark } from "./themes"
 
-const browserHistory = createBrowserHistory()
+//const browserHistory = createBrowserHistory()
 
 const routes = <ThemeProvider theme={ Dark }>
-	<Router history={ browserHistory }>
+	<Router>
 		<Route exact component={ App } />
 	</Router>
 </ThemeProvider>
