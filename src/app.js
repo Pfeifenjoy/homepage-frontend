64097d730f4396d127e1207c1c056ada1331db5b
Arwed Mett <arwed.mett@googlemail.com>
2018-01-21 00:49:57 +0100

    added no script warning

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@
 import React from "react"
 import { Header, Projects } from "./components"
 import { Body, Section } from "./elements"
+import NoscriptWarning from "./noscript"
 
 
 export default () => <Body>
+	<NoscriptWarning />
 	<Header />
 	<Section title="My Projects">
 		<Projects />
